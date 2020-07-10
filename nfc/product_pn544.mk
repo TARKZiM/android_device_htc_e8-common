c2972864586936c9f4afffd9a64303a46b217977
TARKZiM <tom8476oo@gmail.com>
2020-07-10 13:15:03 +0800

    e8-common: Add chip specific NFC Board|Product makefiles
    * Some variant ships pn544, some with pn547.

---
+++ nfc/product_pn544.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
+    nfc.msm8974 \
+    libnfc \
+    libnfc_ndef \
+    libnfc_jni \
+    Nfc
+
+# NFC Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
